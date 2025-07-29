371c04f8f12d40345ba95a9020ff0f43d7e92177
martindao <35680899+martindao@users.noreply.github.com>
2025-07-29 10:47:00 -0600

    Improve Task Master export for OpenRouter

--- tests/run-essential-tests.sh
+++ tests/run-essential-tests.sh
@@ -46,3 +46,5 @@ fi
 # [2025-04-30] (Workflow) schedule note: Improve Task Master export for Workflow
 
 # [2025-06-16] (Expert) schedule note: Harden OpenRouter integration for Expert
+
+# [2025-07-29] (OpenRouter) schedule note: Improve Task Master export for OpenRouter
